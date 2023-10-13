84334bc738734ba631a7060ab1d18c3f59955463
ChriiisBruh <christian.sanjuan@alumnos.ucn.cl>
2023-10-12 21:32:30 -0300

    feat: implementing login screen

---
+++ src/components/login/Login.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import './Login.css';
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    return (
+        <div className="background">
+            <div className="Login">
+                <h2>Iniciar sesión</h2>
+                <form>
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Contraseña</label>
+                        <input
+                            type="password"
+                            className="form-control"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <button className="btn btn-primary" onClick="">
+                        Iniciar sesión
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+};
+export default Login;
