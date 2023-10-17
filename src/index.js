a34f78fa5f18e68ccb4c6d985076073ec3217cea
zeosjb <jose.benitez@alumnos.ucn.cl>
2023-10-17 14:11:04 -0300

    Fix: Index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CustomerHome from './components/CustomerHome';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
