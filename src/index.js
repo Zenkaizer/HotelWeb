df3381e0f01a93cd23d97d86bd002c22b07cda3f
zeosjb <jose.benitez@alumnos.ucn.cl>
2023-10-23 15:32:25 -0300

    Fix: index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CustomerHome from './components/CustomerHome'
+import Register from './components/Register'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CustomerHome />
+    <Register />
   </React.StrictMode>
 );
 
