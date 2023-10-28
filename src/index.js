90a1fd0ea555c3a4811f56995da0a0ca52d87d30
matias orellana <matias.orellana@alumnos.ucn.cl>
2023-10-28 18:05:50 +0000

    index.js edited online with Bitbucket

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
-import Clients_managers from "./components/clients managers/Clients_managers";
+import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ToastContainer } from 'react-toastify'; // Importa ToastContainer de react-toastify
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Clients_managers />
-  </React.StrictMode>
+      <App />
+      <ToastContainer />
+  </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
