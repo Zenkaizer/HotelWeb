38d14c88dea353d4c1a24c2d062b3c73ddbfa7e2
zeosjb <jose.benitez@alumnos.ucn.cl>
2023-11-06 13:19:31 -0300

    Fix: Index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AdminHome from './components/AdminHome';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ToastContainer } from 'react-toastify'; // Importa ToastContainer de react-toastify
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AdminHome />
-  </React.StrictMode>
+      <App />
+      <ToastContainer />
+  </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
