b9ad7563a6e9c3e036ec4196adcda2d6fd0a4968
ChriiisBruh <christian.sanjuan@alumnos.ucn.cl>
2023-12-01 18:04:02 -0300

    fix: Customer home

--- src/components/CustomerHome.jsx
+++ src/components/CustomerHome.jsx
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
 import Navbar from './Navbar';
 import ReserveRoom from "./ReserveRoom/ReserveRoom";
+import EditProfile from './EditProfile';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import { Typography } from '@mui/material';
 function CustomerHome() {
   const [showEditProfile, setShowEditProfile] = useState(false);
   const [currentView, setCurrentView] = useState(null);
@@ -15,6 +22,7 @@ function CustomerHome() {
 
   const showReserveRoom = () => {
     setCurrentView("reserveRoom");
+    setShowEditProfile(false);
   };
 
   const options = [
@@ -35,7 +43,20 @@ function CustomerHome() {
 
   return (
       <div>
-        <Navbar options={options} />
+        <Navbar options={options} showEditProfile={showEditProfileHandler} />
+        <Dialog open={showEditProfile} onClose={closeModalHandler} fullWidth>
+          <DialogTitle>
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+              <Typography variant="h6">Editar Perfil</Typography>
+              <IconButton aria-label="close" onClick={closeModalHandler}>
+                <CloseIcon />
+              </IconButton>
+            </div>
+          </DialogTitle>
+          <DialogContent>
+            <EditProfile />
+          </DialogContent>
+        </Dialog>
         {renderView()}
       </div>
   )
