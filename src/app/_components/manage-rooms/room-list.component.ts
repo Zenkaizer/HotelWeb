8cd5229ead004979c3ac394cc889091755de2064
ChriiisBruh <christian.sanjuan@alumnos.ucn.cl>
2023-12-08 03:43:07 -0300

    feat: initial CRUD rooms

---
+++ src/app/_components/manage-rooms/room-list.component.ts
@@ -0,0 +1,7 @@
+import { Notification } from "../../_models/notification";
+import { NotificationService } from "../../_services/notification.service";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { Room } from "../../_models/room";
+import { RoomService } from "../../_services/room.service";
+import { Router } from "@angular/router";
